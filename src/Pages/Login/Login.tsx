ab5f4d97c0f7adfcf52a21fe624fdc149d38c46e
Sorousch <soroush.raw@gmail.com>
2023-05-16 19:18:43 +0200

    Create Login page component
    Create Login page component with a card on the left with register
    button & on the right with login form and forgot password link.

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Join Our Community</h1>
+          <p>
+            Come and be a part of our community where you can connect with
+            people, share your thoughts and ideas, and learn from each other.
+          </p>
+          <span>Don't have an account yet?</span>
+          <Link to="/register">
+            <button className="register-btn">Create Account</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Welcome Back</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button className="login-btn">Login</button>
+          </form>
+          <div className="forgot-password">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
