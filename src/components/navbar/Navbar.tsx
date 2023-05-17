39746efda94b6ff95fc0beff702b8094578ed792
Sorousch <soroush.raw@gmail.com>
2023-05-17 13:18:20 +0200

    Added icons and search bar to Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import { MdOutlineDarkMode } from "react-icons/md";
+import { BsSun } from "react-icons/bs";
+import { BsGrid } from "react-icons/bs";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { AiOutlineMail } from "react-icons/ai";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+import Logo from "../../assets/Logo.png";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img src={Logo} alt="logo" />
+        </Link>
+        <MdOutlineDarkMode />
 
-export default Navbar
+        <BsSun />
+
+        <BsGrid />
+        <div className="search">
+          <IoSearchOutline />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <IoPersonOutline />
+        <AiOutlineMail />
+        <IoIosNotificationsOutline />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <span>Patrick Müller</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
