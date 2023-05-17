73d9ec8a8dde4982e6c921bc28d0674c514b43df
Sorousch <soroush.raw@gmail.com>
2023-05-17 09:56:13 +0200

    Refactor App.tsx for layout and routes separation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 import "./App.scss";
 import Login from "./Pages/Login/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./Pages/Register/Register";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/LeftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./Pages/Home/Home";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
