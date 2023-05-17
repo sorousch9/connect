2863c8dd703195e05a21e9361b55bfa60c729aa9
Sorousch <soroush.raw@gmail.com>
2023-05-17 10:56:27 +0200

    Refactored routing structure
    Changed the routing structure to use the new react-router-dom v6 API with protected routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,65 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Outlet,
-} from "react-router-dom";
-import "./App.scss";
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
+import RightBar from "./components/rightBar/RightBar";
+import LeftBar from "./components/LeftBar/LeftBar";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import Navbar from "./components/navbar/Navbar";
-import LeftBar from "./components/LeftBar/LeftBar";
-import RightBar from "./components/rightBar/RightBar";
-import Home from "./Pages/Home/Home";
 
-function App() {
-  const Layout = () => {
-    return (
-      <div>
-        <Navbar />
-        <div style={{ display: "flex" }}>
-          <LeftBar />
-          <div style={{ flex: 6 }}>
-            <Outlet />
-          </div>
-          <RightBar />
-        </div>
+const AppLayout = () => (
+  <div>
+    <Navbar />
+    <div style={{ display: "flex" }}>
+      <LeftBar />
+      <div style={{ flex: 6 }}>
+        <Outlet />
       </div>
-    );
-  };
+      <RightBar />
+    </div>
+  </div>
+);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
-}
+const ProtectedRoute = ({ children }: { children: JSX.Element })  => {
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <AppLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const App = () => (
+  <div>
+    <RouterProvider router={router} />
+  </div>
+);
 
 export default App;
