ca007316609f95d8419ff9fbc9711b48d814d6e3
Sorousch <soroush.raw@gmail.com>
2023-05-22 14:54:27 +0200

    Added react-slick and slick-carousel as dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import "./index.scss";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/themeContext";
 import { AuthContextProvider } from "./context/authContext";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
--- types/css.d.ts
+++ types/css.d.ts
@@ -6,7 +6,6 @@ declare module "*.sass" {
   const css: { [key: string]: string };
   export default css;
 }
-declare module "react-markup";
 declare module "*.webp";
 declare module "*.png";
 declare module "*.jpg";
