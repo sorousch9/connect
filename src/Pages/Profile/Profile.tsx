4e61f3890901b63e4337f275a4fb91c2996506cb
Sorousch <soroush.raw@gmail.com>
2023-05-23 10:12:00 +0200

    Added Profile page and styles

---
+++ src/Pages/Profile/Profile.tsx
@@ -0,0 +1,75 @@
+import "./Profile.scss";
+import {
+  SlSocialFacebook,
+  SlSocialInstagram,
+  SlSocialLinkedin,
+  SlSocialTwitter,
+  SlSocialGithub,
+  SlEnvolopeLetter,
+} from "react-icons/sl";
+import { HiOutlineLanguage } from "react-icons/hi2";
+import { MdOutlinePlace } from "react-icons/md";
+import { FiMoreVertical } from "react-icons/fi";
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/1271619/pexels-photo-1271619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="coverPhoto"
+        />
+        <img
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="Detail">
+          <div className="leftside">
+            <div className="social">
+              <a href="http://facebook.com">
+                <SlSocialFacebook fontSize="large" />
+              </a>
+              <a href="http://instagram.com">
+                <SlSocialInstagram fontSize="large" />
+              </a>
+              <a href="http://linkedin.com">
+                <SlSocialLinkedin fontSize="large" />
+              </a>
+              <a href="http://twitter.com">
+                <SlSocialTwitter fontSize="large" />
+              </a>
+              <a href="http://github.com">
+                <SlSocialGithub fontSize="large" />
+              </a>
+            </div>
+            <span>detail 1</span>
+            <span>detail 2</span>
+          </div>
+
+          <div className="profileDetails">
+            <span>Patrick Müller</span>
+            <div className="info">
+              <div className="item">
+                <MdOutlinePlace />
+                <span>Germany</span>
+              </div>
+              <div className="item">
+                <HiOutlineLanguage />
+                <span>Deutsch</span>,<span>Englisch</span>
+              </div>
+            </div>
+          </div>
+          <div className="moreDetail">
+            <SlEnvolopeLetter />
+            <FiMoreVertical />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
