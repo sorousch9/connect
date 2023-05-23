55df440de2d93af9d505eeced339739b9097460c
Sorousch <soroush.raw@gmail.com>
2023-05-23 09:44:32 +0200

    Refactored Navbar layout using Ant Design grid system

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -12,12 +12,13 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { DarkModeContext } from "../../context/themeContext";
 import { AuthContext } from "../../context/authContext";
+import { Col, Row } from "antd";
 const Navbar = () => {
   const { toggle, darkMode } = useContext(DarkModeContext);
   const { currentUser } = useContext(AuthContext);
   return (
-    <div className="navbar">
-      <div className="left">
+    <Row className="navbar">
+      <Col className="left" md={4}>
         <Link to="/">
           {darkMode ? (
             <img src={LogoInDark} alt="logo" />
@@ -25,30 +26,30 @@ const Navbar = () => {
             <img src={LogoInWhite} alt="logo" />
           )}
         </Link>
-      </div>
-      <div className="center">
-        <BsGrid />
+      </Col>
+      <Col className="center" md={16}>
+        <BsGrid size={"1.5rem"} />
         {darkMode ? (
-          <BsSun onClick={toggle} />
+          <BsSun onClick={toggle} size={"1.5rem"} />
         ) : (
-          <MdOutlineDarkMode onClick={toggle} />
+          <MdOutlineDarkMode onClick={toggle} size={"1.5rem"} />
         )}
 
         <div className="search">
           <IoSearchOutline />
           <input type="text" placeholder="Search..." />
         </div>
-      </div>
-      <div className="right">
-        <IoPersonOutline />
-        <AiOutlineMail />
-        <IoIosNotificationsOutline />
+      </Col>
+      <Col className="right" md={4}>
+        <IoPersonOutline size={"1.5rem"} />
+        <AiOutlineMail size={"1.5rem"} />
+        <IoIosNotificationsOutline size={"1.6rem"} />
         <div className="user">
           <img src={currentUser?.profilePic} alt="" />
           <span>{currentUser?.name}</span>
         </div>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 };
 
