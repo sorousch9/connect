b577168c59c2b3b1aa907ecdedb4e054448fcbb0
Sorousch <soroush.raw@gmail.com>
2023-05-25 18:27:04 +0200

    Added input change and register button functionality to Register page

--- connect-client/src/Pages/Register/Register.tsx
+++ connect-client/src/Pages/Register/Register.tsx
@@ -1,7 +1,37 @@
 import { Link } from "react-router-dom";
 import "./Register.scss";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const [err, setErr] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        setErr(error.response?.data);
+      } else {
+        setErr(null);
+      }
+    }
+  };
+
+  
   return (
     <div className="register">
       <div className="card">
@@ -19,11 +49,34 @@ const Register = () => {
         <div className="right">
           <h1>Create an Account</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button className="register-btn">Register</button>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <button className="register-btn" onClick={handleClick}>
+              {err && err}
+              Register
+            </button>
           </form>
         </div>
       </div>
