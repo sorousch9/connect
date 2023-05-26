6a5e4043065808be575575fab4ca21cdcf9b1ec8
Sorousch <soroush.raw@gmail.com>
2023-05-26 10:24:15 +0200

    Add error handling and input state to Login form
    Added error handling for login form and added state for username and password inputs.

--- connect-client/src/Pages/Login/Login.tsx
+++ connect-client/src/Pages/Login/Login.tsx
@@ -1,13 +1,36 @@
-import { Link } from "react-router-dom";
-import "./Login.scss";
-import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useContext, ChangeEvent, FormEvent } from "react";
 import { AuthContext } from "../../context/authContext";
+import "./Login.scss";
+interface Inputs {
+  username: string;
+  password: string;
+}
 
-const Login = () => {
+const Login: React.FC = () => {
   const { login } = useContext(AuthContext);
-  const handleLogin = () => {
-    login();
+  const [inputs, setInputs] = useState<Inputs>({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/");
+    } catch (err: any) {
+      setErr(err.response.data);
+    }
+  };
+
   return (
     <div className="login">
       <div className="card">
@@ -25,15 +48,25 @@ const Login = () => {
         <div className="right">
           <h1>Welcome Back</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={inputs.username}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={inputs.password}
+              onChange={handleChange}
+            />
+            {err && err}
             <button className="login-btn" onClick={handleLogin}>
               Login
             </button>
           </form>
-          <div className="forgot-password">
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
         </div>
       </div>
     </div>
