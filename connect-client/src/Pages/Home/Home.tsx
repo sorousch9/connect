a19396df44133b89a11b761dc728b1297960af48
Sorousch <soroush.raw@gmail.com>
2023-05-30 10:28:54 +0200

    Added Share component to Home page

--- connect-client/src/Pages/Home/Home.tsx
+++ connect-client/src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Events from "../../components/Events/Events";
 import LastActions from "../../components/LastActions/LastActions";
+import Share from "../../components/share/Share";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Events />
+      <Share/>
       <LastActions />
     </div>
   );
